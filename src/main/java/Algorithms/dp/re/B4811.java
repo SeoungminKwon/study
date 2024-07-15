8bcea5a3c57105fcaf031019cf9b9f4c83be7b6f
SeoungminKwon <tjdalsrnjs10@gmail.com>
2024-07-15 14:01:01 +0900

    B12852

--- src/main/java/Algorithms/dp/re/B4811.java
+++ src/main/java/Algorithms/dp/re/B4811.java
@@ -1,53 +1,46 @@
 package Algorithms.dp.re;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class B4811 {
+
     static long[][] dp = new long[31][31];
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
         while (true) {
             int n = sc.nextInt();
-            if (n == 0) {
-                break;
-            }
+            if(n == 0) break;
 
-            //초기화
+            // 초기화
             for (int i = 0; i <= 30; i++) {
-                for (int j = 0; j <= 30; j++) {
-                    dp[i][j] = 0;
-                }
+                Arrays.fill(dp[i], 0);
             }
 
             System.out.println(go(n, 0));
         }
     }
 
-    static long go(int whole, int notWHole) {
-        // 기저사례
-        if (whole == 0 && notWHole == 0) {
-            return 1;
-        }
+    static long go(int whole, int notWhole) {
+        // 기저 사례
+        if(whole == 0 && notWhole == 0)return 1;
 
         //메모리제이션
-        if (dp[whole][notWHole] != 0) {
-            return dp[whole][notWHole];
-        }
+        if(dp[whole][notWhole] != 0)return dp[whole][notWhole];
 
         //로직
         long ret = 0;
+
+        if (notWhole > 0) {
+            ret += go(whole, notWhole - 1);
+        }
+
         if (whole > 0) {
-            ret += go(whole - 1, notWHole + 1);
+            ret += go(whole -1, notWhole + 1);
         }
 
-        if (notWHole > 0) {
-            ret += go(whole, notWHole - 1);
-        }
-
-        dp[whole][notWHole] = ret;
+        dp[whole][notWhole] += ret;
         return ret;
-
     }
 }
