f55264bd9bdf31fc04323394141c7103498b07ca
ksm <tjdalsrnjs10@gmail.com>
2025-04-08 02:13:12 +0900

    2025/04/07 문제풀이

--- src/main/java/codingtest/baekjoon/bruthforce/gold/B15686.java
+++ src/main/java/codingtest/baekjoon/bruthforce/gold/B15686.java
@@ -5,41 +5,100 @@ import java.util.*;
 
 
 public class B15686 {
-
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
     static int n, m;
     static int[][] arr = new int[53][53];
-    static List<Pair> homeList = new ArrayList<>();
-    static List<Pair> chickenList = new ArrayList<>();
-    static int[] visitedChicken = new int[15];
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
     static int result = Integer.MAX_VALUE;
-    public static void main(String args[]) throws IOException {
-
+    static List<Pair> chickenList = new ArrayList<>();
+    static List<Pair> homeList = new ArrayList<>();
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 도시의 치킨거리는 모든 집의 치킨 거리의 합
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < n; i++) {
-            input = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                Pair pair = new Pair(i, j);
-                int number = Integer.parseInt(input[j]);
-                if (number == 1) {
-                    homeList.add(pair);
-                } else if (number == 2) {
-                    chickenList.add(pair);
+                int num = Integer.parseInt(st.nextToken());
+                if (num == 2) {
+                    chickenList.add(new Pair(i, j));
+                } else if (num == 1) {
+                    homeList.add(new Pair(i, j));
+                    arr[i][j] = num;
+                }else{
+                    arr[i][j] = num;
                 }
-                arr[i][j] = number;
             }
         }
 
-        List<Integer> reduceList = new ArrayList<>();
-        reduceChicken(reduceList);
+        chose(new ArrayList<Integer>(), 0);
         System.out.println(result);
 
+
+
+    }
+
+    static void chose(List<Integer> idxList, int start) {
+        if (idxList.size() == m) {
+            int[][] copyArr = new int[53][53];
+            for (int i = 0; i < n; i++) {
+                System.arraycopy(arr[i], 0, copyArr[0], 0, arr[i].length);
+            }
+
+            try {
+                for (int idx : idxList) {
+                    Pair chosenChicken = chickenList.get(idx);
+                    copyArr[chosenChicken.y][chosenChicken.x] = 2;
+                }
+            } catch (Exception e) {
+                System.out.println("here");
+            }
+
+
+
+            int street = 0;
+            for (Pair home : homeList) {
+                int[][] visited = new int[53][53];
+                visited[home.y][home.x] = 1;
+
+                Queue<Pair> q = new LinkedList<>();
+                q.add(home);
+
+                while (!q.isEmpty()) {
+                    Pair pair =  q.poll();
+
+                    for (int i = 0; i < 4; i++) {
+                        int ny = pair.y + dy[i];
+                        int nx = pair.x + dx[i];
+
+                        if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+                        if(visited[ny][nx] != 0) continue;
+                        if(copyArr[ny][nx] == 1) continue;
+
+                        visited[ny][nx] = visited[pair.y][pair.x] + 1;
+                        q.add(new Pair(ny, nx));
+                    }
+
+                }
+
+                int min = Integer.MAX_VALUE;
+                for (int idx : idxList) {
+                    Pair chosenChicken = chickenList.get(idx);
+                    min = Math.min(min, visited[chosenChicken.y][chosenChicken.x]);
+                }
+
+                street += min;
+            }
+            result = Math.min(result, street);
+        }else{
+            for (int i = start; i < chickenList.size(); i++) {
+                idxList.add(Integer.valueOf(i));
+                chose(idxList, start + 1);
+                idxList.remove(Integer.valueOf(i));
+            }
+        }
     }
 
     static class Pair{
@@ -51,56 +110,4 @@ public class B15686 {
             this.x = x;
         }
     }
-
-    static void reduceChicken(List<Integer> reduceList) {
-        if (reduceList.size() == m) {
-            calculateStreet(reduceList);
-            return;
-        }else{
-            for (int i = 0; i < chickenList.size(); i++) {
-                if(visitedChicken[i] != 0) continue;
-                visitedChicken[i] = 1;
-                reduceList.add(i);
-                reduceChicken(reduceList);
-                visitedChicken[i] = 0;
-            }
-        }
-    }
-
-    static void calculateStreet(List<Integer> reduceList) {
-        for (int i = 0; i < homeList.size(); i++) {
-            Pair pair = homeList.get(i);
-            Queue<Pair> q = new LinkedList<>();
-            int[][] visited = new int[53][53];
-            for (int[] init : visited) {
-                Arrays.fill(init, Integer.MAX_VALUE);
-            }
-
-            q.add(pair);
-            visited[pair.y][pair.x] = 1;
-
-            while (q.size() != 0) {
-                Pair now = q.poll();
-                for (int j = 0; j < 4; j++) {
-                    int ny = now.y + dy[j];
-                    int nx = now.x + dx[j];
-
-                    if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-                    if(arr[ny][nx] != 1) continue;
-                    if(visited[ny][nx] != Integer.MAX_VALUE) continue;
-
-                    visited[ny][nx] = visited[now.y][now.x] + 1;
-                    q.add(new Pair(ny, nx));
-                }
-            }
-
-            int sum = 0;
-            for (int j = 0; j < reduceList.size(); j++) {
-                Pair chickenPair = chickenList.get(reduceList.get(j));
-                sum += visited[chickenPair.y][chickenPair.x];
-            }
-            result = Math.min(result, sum);
-        }
-    }
-
 }
--- src/main/java/codingtest/baekjoon/dfs_bfs/silver/B2910.java
+++ src/main/java/codingtest/baekjoon/dfs_bfs/silver/B2910.java
@@ -4,53 +4,50 @@ import java.io.*;
 import java.util.*;
 
 public class B2910 {
-
     static int n, c;
-    public static void main(String args[]) throws Exception{
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        c = Integer.parseInt(input[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
+        st = new StringTokenizer(br.readLine());
         Map<Integer, Integer> map = new LinkedHashMap<>();
-        input = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            int temp = Integer.parseInt(input[i]);
-            map.put(temp, map.getOrDefault(temp, 0) + 1);
+            int temp = Integer.parseInt(st.nextToken());
+            int value = map.getOrDefault(temp, 0);
+            map.put(temp, value + 1);
         }
 
         List<Pair> pairList = new ArrayList<>();
-        for(Map.Entry<Integer, Integer> entry  :  map.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             pairList.add(new Pair(entry.getKey(), entry.getValue()));
         }
 
-        Collections.sort(pairList, new Comparator<Pair>() {
-
-            public int compare(Pair p1, Pair p2){
+        Collections.sort(pairList, new Comparator<Pair>(){
+            public int compare(Pair p1, Pair p2) {
                 return p2.cnt - p1.cnt;
             }
         });
 
         StringBuilder sb = new StringBuilder();
-        for (Pair pair : pairList) {
-            for (int i = 0; i < pair.cnt; i++) {
-                sb.append(pair.num);
-                sb.append(" ");
+        for (Pair p : pairList) {
+            for (int i = 0; i < p.cnt; i++) {
+                sb.append(p.number).append(" ");
             }
         }
 
         System.out.println(sb.toString().trim());
-    }
 
+
+    }
     static class Pair{
-        int num;
+        int number;
         int cnt;
 
-        Pair(int num, int cnt) {
-            this.num = num;
+        Pair(int number, int cnt) {
+            this.number = number;
             this.cnt = cnt;
         }
     }
-
-
 }
